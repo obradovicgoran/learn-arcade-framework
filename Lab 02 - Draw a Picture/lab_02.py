5db960d248c6b49255e7bf23df6fa9d81941429e
goranobradovic <goranobradovic@icloud.com>
2022-11-10 10:36:16 +0100

    Test for rendering an Arcade window

---
+++ Lab 02 - Draw a Picture/lab_02.py
@@ -0,0 +1,8 @@
+import arcade
+
+arcade.open_window(600, 900, "Drawing Example")
+arcade.set_background_color(arcade.csscolor.AQUAMARINE)
+arcade.start_render()
+arcade.finish_render()
+
+arcade.run()
